ec931f6c1452a064f4f160c3ae217a55c9bec3a1
angeldesweb <angeldesweb@mail.com>
2022-11-24 19:43:56 -0400

    fix model product

--- src/models/store/Product.js
+++ src/models/store/Product.js
@@ -1,20 +1,22 @@
 import { Schema , model } from 'mongoose';
 
 const Product = new Schema({
-    code:{type:String,required:true,unique:true}, //Código de barras
-    brand:{type:Schema.ObjectId,ref:'Brand'},
-    category:{type:Schema.ObjectId,ref:'SubCategory'},
-    name:{type:String,required:true,unique:true},
-    slug:{type:String,required:true},
-    price:{type:Number,default:0},
+    code:{type:String,required:true,unique:true},//---  //Código de barras
+    cover:{type:String,required:true}, 
+    brand:{type:Schema.ObjectId,ref:'Brand'}, //---
+    sub:{type:Schema.ObjectId,ref:'SubCategory'}, //----
+    name:{type:String,required:true,unique:true}, //----
+    slug:{type:String,required:true}, //----
+    price:{type:Number,default:0}, //----
+    //--Sin agregar en productos agregar en update --//
     stock:{type:Number,default:0}, //Input number
+    updatedAt:{type:Date},
     //---NO SE AGREGAN --//
-    status:{type:Boolean,default:false}, //No se coloca de momento
-    //extracto:{type:String}, //
-    cover:{type:String,required:true}, //No se agregaría de momento
+    //No se coloca de momento
+    extracto:{type:String}, // ?
     discount:{type:Boolean,default:false}, //No se gregaria
-    createdAt:{type:Date,default:Date.now()}, //Se crea por defecto
-    updatedAt:{type:Date}
+    status:{type:Boolean,default:false}, 
+    createdAt:{type:Date,default:Date.now()} //Se crea por defecto
 });
 
 export default model('Product',Product);
